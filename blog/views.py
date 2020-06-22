80434a81ae30b87f75ca1696a8a01c15deeea263
GoktugYalcin <goktugyalcin06@gmail.com>
2020-06-22 12:50:05 +0300

    Veritabanından veri çekimi

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(tarihYayinlama__lte=timezone.now()).order_by('tarihYayinlama')
+    return render(request, 'blog/post_list.html', {'posts': posts})
